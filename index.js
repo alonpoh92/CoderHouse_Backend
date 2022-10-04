09854b2472137a6bff9869f30ad6917132e81da2
Alonso Aponte <alonpoh92@hotmail.com>
2022-10-04 01:50:44 -0500

    Desafio 6 - Websockets

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Server: HttpServer } = require('http');
 const { Server: IOServer } = require('socket.io');
-const {Contenedor} = require('./classes/Contenedor');
+const {Contenedor} = require('./model/classes/Contenedor');
 
 const productos = new Contenedor('productos.txt');
 const mensajes = new Contenedor('mensajes.txt');
