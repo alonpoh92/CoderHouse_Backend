7433a42a9cd28a0cebae1157915505451fee563a
Alonso Aponte <alonpoh92@hotmail.com>
2022-12-05 21:09:57 -0500

    Desafio 10 - LogIn por Formulario

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const { Server: HttpServer } = require('http');
 const { Server: IOServer } = require('socket.io');
-const { Container } = require('./model/classes/Container');
-const dbConfig = require('./db/config');
-
-const products = new Container(dbConfig.mariaDB, 'products');
-const messages = new Container(dbConfig.sqlite, 'messages');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
