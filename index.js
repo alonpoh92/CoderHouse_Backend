ae878720ad1c3dfa99558d8c1e2f93e9e702e265
Alonso Aponte <alonpoh92@hotmail.com>
2022-12-05 21:25:36 -0500

    Desafio 11 - LogIn por Formulario

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { Server: HttpServer } = require('http');
 const { Server: IOServer } = require('socket.io');
 const envConfig = require('./config');
+const appRoutes = require('./routes/app.routes');
 
 const PORT = envConfig.SERVER_PORT || 8080;
 const app = express();
