cba4425932c60fd90e3dfa4324c449aaed323110
Alonso Aponte <alonpoh92@hotmail.com>
2022-12-11 23:09:55 -0500

    Desafio 11 - LogIn por Formulario

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,7 @@
 const signUp = document.getElementById('signUp');
 const imgLogIn = document.getElementById('imgLogIn');
 const signUpBtn = document.getElementById('signUpBtn');
+const signInBtn = document.getElementById('signInBtn');
 
 function showSignUp(){
     imgLogIn.classList.remove("signin");
@@ -21,56 +22,108 @@ function checkEmail(id, idMsg){
     const email = document.getElementById(id);
     const emailMsg = document.getElementById(idMsg);
     if(email.value.trim().match(validRegex)){
-        emailMsg.classList.add('d-none');
+        if(idMsg){
+            emailMsg.classList.add('d-none');
+        }
         return true;
     }else{
-        emailMsg.classList.remove('d-none');
+        if(idMsg){
+            emailMsg.classList.remove('d-none');
+        }
         return false;
     }
 }
 
-function validPassword(id, idMsg){
-    const validRegex = ^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]);
+function checkPassword(id, idMsg){
+    const validRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/;
     const password = document.getElementById(id);
     const passwordMsg = document.getElementById(idMsg);
     if(password.value.trim().match(validRegex)){
-        passwordMsg.classList.add('d-none');
+        if(idMsg){
+            passwordMsg.classList.add('d-none');
+        }
         return true;
     }else{
-        passwordMsg.classList.remove('d-none');
+        if(idMsg){
+            passwordMsg.classList.remove('d-none');
+        }
         return false;
     }
 }
 
-function validLong(id, long={}, idMsg){
+function validateLong(id, long={}, idMsg){
     let validMin = true;
     let validMax = true;
     const element = document.getElementById(id);
     const elementMsg = document.getElementById(idMsg);
     if(long.min){
-        if(password.value.trim().length < long.min){
+        if(element.value.trim().length < long.min){
             validMin = false;
         }
     }
     if(long.max){
-        if(password.value.trim().length > long.max){
+        if(element.value.trim().length > long.max){
             validMax = false;
         }
     }
     const valid = validMin && validMax;
     if(valid){
-        elementMsg.classList.add('d-none');
+        if(idMsg){
+            elementMsg.classList.add('d-none');
+        }
         return true;
     }else{
-        elementMsg.classList.remove('d-none');
+        if(idMsg){
+            elementMsg.classList.remove('d-none');
+        }
         return false;
     }
 }
 
+function validateSame(ids=[], idMsg){
+    const elementMsg = document.getElementById(idMsg);
+    let pass = '';
+    for(let id in ids){
+        const val = document.getElementById(ids[id]);
+        if(id > 0){
+            if(val.value.trim() != pass){
+                if(idMsg){
+                    elementMsg.classList.remove('d-none');
+                }
+                return false;
+            }
+        }else{
+            pass = val.value.trim();
+        }
+    }
+    if(idMsg){
+        elementMsg.classList.add('d-none');
+    }
+    return true;
+}
+
 function validateSignUp(){
-    if(checkEmail('emailSignUp', 'validEmail') && validPassword('password1SignUp', 'passwordCharacter') && validLong('password1SignUp', {min: 8, max: 20}, 'passwordLong')){
+    const validEmail = checkEmail('emailSignUp', 'validEmail');
+    const validPassword = checkPassword('password1SignUp', 'passwordCharacter');
+    const validPasswordLong = validateLong('password1SignUp', {min: 8, max: 20}, 'passwordLong');
+    const validName = validateLong('name', {min: 1}, 'validName');
+    const validSamePass = validateSame(['password1SignUp', 'password2SignUp'], 'passwordSame')
+
+    if(validEmail && validPassword && validPasswordLong && validName && validSamePass){
         signUpBtn.classList.remove('disabled');
+        document.getElementById('validPassword').classList.add('d-none');
     }else{
         signUpBtn.classList.add('disabled');
+        document.getElementById('validPassword').classList.remove('d-none');
+    }
+}
+
+function validateSignIn(){
+    const validEmail = checkEmail('emailSignIn');
+    const validPasswordLong = validateLong('passwordSignIn', {min: 1})
+    if(validEmail && validPasswordLong){
+        signInBtn.classList.remove('disabled');
+    }else{
+        signInBtn.classList.add('disabled');
     }
 }
