a04f6566a6c94403fcc8412f3c832c8df4f4b21d
Alonso Aponte <alonpoh92@hotmail.com>
2022-12-27 12:38:28 -0500

    Desafio 14 - Objeto Process

--- routers/api/api.routes.js
+++ routers/api/api.routes.js
@@ -6,4 +6,9 @@ const router = express.Router();
 //Routes
 router.use('/auth', authRoutes);
 
+router.get('/randoms', (req, res) => {
+    const cant = req.query.cant || 100000000;
+    res.json({cant});
+})
+
 module.exports = router;
